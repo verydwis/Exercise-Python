f9bf04868012ef2cdb1b3151e41c782a7f76aef2
very dwi setiawan <verydwisetiawan@192.168.1.3>
2025-01-26 22:37:32 +0700

    first commit

---
+++ PalindromeNumber.py
@@ -0,0 +1,11 @@
+def isPalindrome( x):
+    if x <0 or (x !=0 and x % 10 == 0):
+        return False
+    half =0
+    while x > half:
+        half = (half*10)+(x%10)
+        x = x //10
+    return x == half or x == half // 10
+
+x = 121
+print(x, "what is Palindrome? :", isPalindrome(x))
